fd60acf06a786bb799fb4ad2a5e294ff0b99692d
julianbooher <julianbooher@hotmail.com>
2020-12-20 12:20:13 -0600

    rendering genres, don't feel good about this either

--- src/components/MovieGalleryDetails/MovieGalleryDetails.js
+++ src/components/MovieGalleryDetails/MovieGalleryDetails.js
@@ -10,21 +10,39 @@ class MovieGalleryDetails extends Component {
       this.props.history.push('/')
    }
 
+   createGenreString = (genreArray) => {
+      let returnString = '| '
+      if (genreArray !== undefined){
+         if (genreArray.length === 0){
+            return 'None Listed'
+         }
+         for (let genre of genreArray){
+            returnString += ` ${genre.name} |`
+         }
+         return returnString;
+      }
+   }
+
    render() {
       // This holds the movie details (title, image, description).
       const currentMovie = this.props.reduxState.details[0]
-      // This holds the genres.
+      // This holds the genres in an array.
       const currentMovieGenres = this.props.reduxState.details[1]
+      // Variable to put in the return that displays the genres.
+      let genreString = this.createGenreString(currentMovieGenres);
       console.log('currentMovie const', currentMovie)
       console.log('currentMovie const', currentMovieGenres)
 
       return (
          <>
             {/* Without this conditional, the variable is undefined because of how the page loads. */}
-            {currentMovie !== undefined &&
+            {currentMovie !== undefined && currentMovieGenres !== undefined &&
             <div className="movie-details-item">
                <h1>{currentMovie.title}</h1>
                <img src={currentMovie.poster} alt={currentMovie.title}/>
+               <p>Genres</p>
+               <p>{genreString} </p>
+               {/* {currentMovieGenres.map} */}
                <p>{currentMovie.description}</p>
             </div>}
             <button onClick={this.returnToGallery}>Return to Gallery</button>
